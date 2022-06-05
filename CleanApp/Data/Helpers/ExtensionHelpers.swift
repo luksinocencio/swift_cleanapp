38b1f6f91fc9c3ec50b4d58b0f5c79394982945b
luksinocencio <luksinocencio@gmail.com>
2022-06-05 19:18:52 -0300

    test: add data extension to convert to json

--- CleanApp/Data/Helpers/ExtensionHelpers.swift
+++ CleanApp/Data/Helpers/ExtensionHelpers.swift
@@ -4,4 +4,8 @@ public extension Data {
     func toModel<T: Codable>() -> T? {
         try? JSONDecoder().decode(T.self, from: self)
     }
+
+    func toJson() -> [String: Any]? {
+        try? JSONSerialization.jsonObject(with: self, options: .allowFragments) as? [String: Any]
+    }
 }
--- CleanApp/InfraTests/AlamofireAdapterTests.swift
+++ CleanApp/InfraTests/AlamofireAdapterTests.swift
@@ -12,7 +12,7 @@ class AlamofireAdapter {
     }
 
     func post(to url: URL, with data: Data?) {
-        let json = data == nil ? nil : try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+        let json = data?.toJson()
         session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
     }
 }
