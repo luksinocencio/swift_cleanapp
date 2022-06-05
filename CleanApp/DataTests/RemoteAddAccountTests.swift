8723258a56799c82ec6adbf9a359395dc8d61969
luksinocencio <luksinocencio@gmail.com>
2022-06-05 03:13:09 -0300

    chore: move SUT creation to a factory method

--- CleanApp/DataTests/RemoteAddAccountTests.swift
+++ CleanApp/DataTests/RemoteAddAccountTests.swift
@@ -24,25 +24,16 @@ protocol HttpPostClient {
  - All tests have to start name with test
  */
 class RemoteAddAccountTests: XCTestCase {
-
     func test_add_should_call_httpClient_with_correct_utl() {
-        // prepare data
-        guard let url = URL(string: "http://any-url.com") else { return }
-        let httpClientSpy = HttpClientSpy()
-        
-        /// sut ->  system under test
-        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
-        
-        /// call test
+        let url = URL(string: "http://any-url.com")!
+        let (sut, httpClientSpy) = makeSut(url: url)
         sut.add(addAccountModel:  makeAddAccountModel())
-        
-        /// result test
         XCTAssertEqual(httpClientSpy.url, url)
     }
     
     func test_add_should_call_httpClient_with_correct_data() {
-        let httpClientSpy = HttpClientSpy()
-        let sut = RemoteAddAccount(url: URL(string: "http://any-url.com")!, httpClient: httpClientSpy)
+        let (sut, httpClientSpy) = makeSut()
+        sut.add(addAccountModel:  makeAddAccountModel())
         let addAccountModel = makeAddAccountModel()
         sut.add(addAccountModel: addAccountModel)
         let data = try? JSONEncoder().encode(addAccountModel)
@@ -52,11 +43,18 @@ class RemoteAddAccountTests: XCTestCase {
 
 extension RemoteAddAccountTests {
     
-    /// factory
+    //MARK: - Factory(ies).
+    func makeSut(url: URL = URL(string: "http://any-url.com")!) -> (sut: RemoteAddAccount, httpClientSpy: HttpClientSpy) {
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
+        return (sut, httpClientSpy)
+    }
+    
     func makeAddAccountModel() -> AddAccountModel {
         AddAccountModel(name: "any_name", email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
     }
     
+    //MARK: - Spy(ies)
     /// spy -> versão mockado do que precisamos
     class HttpClientSpy: HttpPostClient {
         var url: URL?
