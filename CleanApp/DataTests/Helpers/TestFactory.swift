91f15b49d1bfea3f10a1c8eaeaccff39207e6d4b
luksinocencio <luksinocencio@gmail.com>
2022-06-05 18:45:02 -0300

    test: enusre AlamofireAdapter calls Alamofire with correct data

--- CleanApp/DataTests/Helpers/TestFactory.swift
+++ CleanApp/DataTests/Helpers/TestFactory.swift
@@ -4,6 +4,10 @@ func makeInvalidData() -> Data {
     Data("invalid_data".utf8)
 }
 
+func makeValidData() -> Data {
+    Data("{\"name\":\"Lucas\"}".utf8)
+}
+
 func makeUrl() -> URL {
     URL(string: "http://any-url.com")!
 }
