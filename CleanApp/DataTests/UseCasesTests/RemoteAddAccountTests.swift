61756bd6345a3625a37d11ae43ed814bd09eac5f
luksinocencio <luksinocencio@gmail.com>
2022-06-05 04:51:47 -0300

    test: simplify code with helpers

--- CleanApp/DataTests/UseCasesTests/RemoteAddAccountTests.swift
+++ CleanApp/DataTests/UseCasesTests/RemoteAddAccountTests.swift
@@ -9,7 +9,7 @@ import XCTest
 class RemoteAddAccountTests: XCTestCase {
 
     func test_add_should_call_httpClient_with_correct_utl() {
-        let url = URL(string: "http://any-url.com")!
+        let url = makeUrl()
         let (sut, httpClientSpy) = makeSut(url: url)
         sut.add(addAccountModel:  makeAddAccountModel()) { _ in }
         XCTAssertEqual(httpClientSpy.urls, [url])
@@ -40,7 +40,7 @@ class RemoteAddAccountTests: XCTestCase {
     func test_add_should_complete_with_error_if_client_completes_with_invalid_data() {
         let (sut, httpClientSpy) = makeSut()
         expect(sut, completeWith: .failure(.invalidData), when: {
-            httpClientSpy.completionWithData(Data("invalid_data".utf8))
+            httpClientSpy.completionWithData(makeInvalidData())
         })
     }
 }
@@ -76,6 +76,14 @@ extension RemoteAddAccountTests {
         }
         action()
         wait(for: [exp], timeout: 1)
+    }
+
+    func makeInvalidData() -> Data {
+        Data("invalid_data".utf8)
+    }
+
+    func makeUrl() -> URL {
+        URL(string: "http://any-url.com")!
     }
     
     //MARK: - Spy(ies)
