c88fd300da4d96701b6945d9d954693df56ceca0
luksinocencio <luksinocencio@gmail.com>
2022-06-07 22:00:56 -0300

    test: change SUT creation should return just on object

--- CleanApp/DataTests/Helpers/AccountModelFactory.swift
+++ CleanApp/DataTests/Helpers/AccountModelFactory.swift
@@ -2,5 +2,5 @@ import Foundation
 import Domain
 
 func makeAccountModel() -> AccountModel {
-    AccountModel(id: "id_any", name: "any_name", email: "any_email@mail.com", password: "any_password")
+    AccountModel(id: "id_any", name: "any_name", email: "any_email@mail.com", password: "any_password", accessToken: "any_token")
 }
