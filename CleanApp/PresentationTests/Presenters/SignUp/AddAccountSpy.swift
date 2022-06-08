fb87e448ff428a1783b4aaeeb6aed7da0310a2c5
luksinocencio <luksinocencio@gmail.com>
2022-06-08 19:36:01 -0300

    test: move Spy to own files

---
+++ CleanApp/PresentationTests/Presenters/SignUp/AddAccountSpy.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+@testable import Presentation
+@testable import Data
+@testable import Domain
+
+class AddAccountSpy: AddAccount {
+    var addAccountModel: AddAccountModel?
+    var completion: ((Result<AccountModel, DomainError>) -> Void)?
+
+    func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, DomainError>) -> Void) {
+        self.addAccountModel = addAccountModel
+        self.completion = completion
+    }
+
+    func completeWithError(_ error: DomainError) {
+        completion?(.failure(error))
+    }
+
+    func completeWithAccount(_ account: AccountModel) {
+        completion?(.success(account))
+    }
+}
