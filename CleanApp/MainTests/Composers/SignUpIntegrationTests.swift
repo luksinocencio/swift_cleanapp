3019c81562983e47df50b8d1eac9e37d10f11aed
luksinocencio <luksinocencio@gmail.com>
2023-03-24 12:31:35 -0300

    edit file

--- CleanApp/MainTests/Composers/SignUpIntegrationTests.swift
+++ CleanApp/MainTests/Composers/SignUpIntegrationTests.swift
@@ -3,9 +3,6 @@ import XCTest
 
 final class SignUpIntegrationTests: XCTestCase {
     func test_ui_presentation_integration() {
-        debugPrint("===========================")
-        debugPrint(Environment.variable(.apiBaseUrl))
-        debugPrint("===========================")
         let sut = SignUpComposer.composeControllerWith(addAccount: AddAccountSpy())
         checkMemoryLeak(for: sut)
     }
