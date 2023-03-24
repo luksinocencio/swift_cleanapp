82e352dd52bcdb81a05f566912b4a614018e977d
luksinocencio <luksinocencio@gmail.com>
2023-03-24 01:00:14 -0300

    add environment variables

--- CleanApp/MainTests/SignUpIntegrationTests.swift
+++ CleanApp/MainTests/SignUpIntegrationTests.swift
@@ -2,7 +2,10 @@ import XCTest
 @testable import Main
 
 final class SignUpIntegrationTests: XCTestCase {
-    func testExample() {
+    func test_ui_presentation_integration() {
+        debugPrint("===========================")
+        debugPrint(Environment.variable(.apiBaseUrl))
+        debugPrint("===========================")
         let sut = SignUpComposer.composeControllerWith(addAccount: AddAccountSpy())
         checkMemoryLeak(for: sut)
     }
