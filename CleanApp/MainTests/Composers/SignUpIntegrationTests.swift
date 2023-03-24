6df19c97ff2f33c7c0473940f1e32fa6ab6132a8
luksinocencio <luksinocencio@gmail.com>
2023-03-24 13:26:32 -0300

    move threading logic to the decorator class

--- CleanApp/MainTests/Composers/SignUpIntegrationTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import Main
-
-final class SignUpIntegrationTests: XCTestCase {
-    func test_ui_presentation_integration() {
-        let sut = SignUpComposer.composeControllerWith(addAccount: AddAccountSpy())
-        checkMemoryLeak(for: sut)
-    }
-}
--- CleanApp/Presentation/Presenters/SignUp/SignUpPresenter.swift
+++ CleanApp/Presentation/Presenters/SignUp/SignUpPresenter.swift
@@ -6,14 +6,14 @@ public final class SignUpPresenter {
     private let emailValidator: EmailValidator
     private let addAccount: AddAccount
     private weak var loadingView: (LoadingView)?
-
+    
     public init(alertView: AlertView, emailValidator: EmailValidator, addAccount: AddAccount, loadingView: LoadingView) {
         self.alertView = alertView
         self.emailValidator = emailValidator
         self.addAccount = addAccount
         self.loadingView = loadingView
     }
-
+    
     public func signUp(viewModel: SignUpViewModel) {
         if let message = validate(viewModel: viewModel) {
             alertView?.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: message))
@@ -22,14 +22,13 @@ public final class SignUpPresenter {
             addAccount.add(addAccountModel: SignUpMapper.toAddAccountModel(viewModel: viewModel)) { [weak self] result in
                 guard let self = self else { return }
                 switch result {
-                case .failure: self.alertView?.showMessage(viewModel: AlertViewModel(title: "Erro", message: "Algo inesperado acontenceu, tente novamente em alguns instantes."))
-                case .success: self.alertView?.showMessage(viewModel: AlertViewModel(title: "Sucesso", message: "Conta criada com sucesso."))
+                    case .failure: self.alertView?.showMessage(viewModel: AlertViewModel(title: "Erro", message: "Algo inesperado acontenceu, tente novamente em alguns instantes."))
+                    case .success: self.alertView?.showMessage(viewModel: AlertViewModel(title: "Sucesso", message: "Conta criada com sucesso."))
                 }
-                self.loadingView?.display(viewModel: LoadingViewModel(isLoading: false))
             }
         }
     }
-
+    
     private func validate(viewModel: SignUpViewModel) -> String? {
         if viewModel.name == nil || viewModel.name!.isEmpty {
             return "O campo Nome é obrigatorio"
